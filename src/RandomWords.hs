1a0c1aacc4b2995226e92e91a42b4718ec127371
Lex. Huang <9404826+huanhulan@users.noreply.github.com>
2020-04-05 16:04:57 +0800

    test: using QuickCheck

--- src/RandomWords.hs
+++ src/RandomWords.hs
@@ -4,11 +4,13 @@ import System.Random(randomRIO)
 
 type WordList = [String]
 
-getMinWordLength :: [String] -> Int
-getMinWordLength list = minimum (fmap length list)
+getMinWordLength :: [String] -> Maybe Int
+getMinWordLength []   = Nothing
+getMinWordLength list = Just $ minimum (fmap length list)
 
-getMaxWordLength :: [String] -> Int
-getMaxWordLength list = maximum (fmap length list)
+getMaxWordLength :: [String] -> Maybe Int
+getMaxWordLength []   = Nothing
+getMaxWordLength list = Just $ maximum (fmap length list)
 
 
 allWords :: IO WordList
@@ -23,12 +25,15 @@ gameWords =
     return (filter (filterWords aw) aw)
     where
       filterWords list = gameLength (getMinWordLength list) (getMaxWordLength list)
-      gameLength :: Int -> Int -> String -> Bool
-      gameLength minLen maxLen w =
+      gameLength :: Maybe Int -> Maybe Int -> String -> Bool
+      gameLength Nothing _ _ = False
+      gameLength _ Nothing _ = False
+      gameLength (Just minLen) (Just maxLen) w =
         l >= minLen && l <= maxLen
           where l = length w
 
 randomWord' :: WordList -> IO String
+randomWord' [] = return ""
 randomWord' wl = do
   randomIndex <- randomRIO (0 , length wl - 1)
   return $ wl !!randomIndex
--- test/RandomWordsSpec.hs
+++ test/RandomWordsSpec.hs
@@ -1,18 +1,24 @@
 module RandomWordsSpec where
 import SpecHelper
+import Test.QuickCheck
 import RandomWords
+import Data.Maybe(isNothing)
 
 getMinSpec :: Spec
 getMinSpec =
-    describe "getMinWordLength" $
+    describe "getMinWordLength" $ do
       it "getMinWordLength with ['a', \"ab\", \"abc\" , \"abcd\", \"abcde\"] is 1" $
-        getMinWordLength ["a","ab", "abc" , "abcd", "abcde"] `shouldBe` 1
+        getMinWordLength ["a","ab", "abc" , "abcd", "abcde"] `shouldBe` Just 1
+      it "getMinWordLength will always bigger or equal than zero" $
+        property testGetMin where
+          testGetMin [] = isNothing(getMinWordLength [])
+          testGetMin x = res >= 0 where Just res = getMinWordLength x
 
 getMaxSpec :: Spec
 getMaxSpec =
     describe "getMaxWordLength" $
       it "getMaxWordLength with ['a', \"ab\", \"abc\" , \"abcd\", \"abcde\"] is 5" $
-        getMaxWordLength ["a","ab", "abc" , "abcd", "abcde"] `shouldBe` 5
+        getMaxWordLength ["a","ab", "abc" , "abcd", "abcde"] `shouldBe` Just 5
 
 spec :: Spec
 spec =
